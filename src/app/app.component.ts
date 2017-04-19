38e8543f16a3efae714d67744bb54df7aa851081
nihadkeric <nihadkeric@gmail.com>
2017-04-19 20:40:29 +0200

    View list of contacts and details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+import { Bussinesses} from './model/Businesses';
+import { Category } from './model/Category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  
+  businesses: Bussinesses[];
+  categories: Category[];
+  appState: string;
+  activeKey: string;
+
+  constructor(private _fs: FirebaseService){
+  }
+
+  ngOnInit(){
+    this._fs.getBusinesses().subscribe(bussineses => {
+      this.businesses = bussineses;
+      console.log(bussineses);
+    });
+
+    this._fs.getCategories().subscribe(categories => {
+      this.categories = categories;
+      console.log(categories);
+    });
+  }
+
+  changeState(state, key){
+    console.log("Changing state to: "+ state);
+    
+    if(key){
+      console.log("Changing key to: "+ key);
+      this.activeKey = key;
+    }
+
+    if(state){
+      this.appState = state;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,31 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { HeaderComponent } from './header/header.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyB2w4EjEY812MpjSvz-055lzedZJIbQ5F8",
+    authDomain: "ionic-travel.firebaseapp.com",
+    databaseURL: "https://ionic-travel.firebaseio.com",
+    storageBucket: "ionic-travel.appspot.com",
+    messagingSenderId: "723372099256"
+  };
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
