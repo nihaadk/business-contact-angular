920189719dcdaf5304ab54005caf62c9aaf0fda3
Nihad Kerić <nihadkeric@gmail.com>
2017-05-06 12:35:39 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { AngularFireModule } from 'angularfire2';
 
 // Must export the config
 export const firebaseConfig = {
-    apiKey: "AIzaSyB2w4EjEY812MpjSvz-055lzedZJIbQ5F8",
-    authDomain: "ionic-travel.firebaseapp.com",
-    databaseURL: "https://ionic-travel.firebaseio.com",
-    storageBucket: "ionic-travel.appspot.com",
-    messagingSenderId: "723372099256"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
 
